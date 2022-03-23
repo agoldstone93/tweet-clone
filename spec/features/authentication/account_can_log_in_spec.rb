22fb368a5bbc8038fb93ac4601db4ee631d64516
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-03-23 17:13:50 +0000

    add test for user sign in

--- spec/features/authentication/account_can_log_in_spec.rb
+++ spec/features/authentication/account_can_log_in_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
-describe 'account can log in' do
-  context 'with valid log in details' do
-    it 'takes me to the homepage'
+RSpec.describe "Authentication", type: :feature do
+  describe 'account can log in' do
+    let(:account) { FactoryBot.create(:account) }
+    
+    context 'with valid log in details' do
+      it 'takes me to the homepage' do
+        visit new_account_session_path
+        within('#new_account') do
+          fill_in "Email", with: account.email
+          fill_in "Password", with: account.password
+        end
+        click_button "Log in"
+        expect(page).to have_content "Hello #{account.name}"
+      end
+    end
   end
 end
