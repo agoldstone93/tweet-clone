39d14573157ed6c73dc5821bebd2267fae8cb8e1
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-03-23 15:37:54 +0000

    add timeline/index as a valid route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'profile/index'
+  get 'timeline/index'
   root 'timeline#index'
   get 'timeline', to: "timeline#index"
   get 'profile', to: "profile#index"
