5906a59e7c63bdba1ef5548337e5236a3ec6fef1
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-03-24 17:02:19 +0000

    clean up tests for account log in

--- spec/features/authentication/account_can_log_in_spec.rb
+++ spec/features/authentication/account_can_log_in_spec.rb
@@ -1,28 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "Authentication", type: :feature do
+describe "Authentication" do
   describe 'account can log in' do
     let(:account) { FactoryBot.create(:account) }
-    
-    context 'with valid log in details' do
-      it 'takes me to the homepage' do
-        visit new_account_session_path
-        within('#new_account') do
-          fill_in "Email", with: account.email
-          fill_in "Password", with: account.password
-        end
-        click_button "Log in"
-        expect(page).to have_current_path(root_path)
-      end
-    end
-    
+
     context 'with valid log in details' do
       it 'displays welcome message' do
         visit new_account_session_path
-        within('#new_account') do
-          fill_in "Email", with: account.email
-          fill_in "Password", with: account.password
-        end
+        fill_in "Email", with: account.email
+        fill_in "Password", with: account.password
         click_button "Log in"
         expect(page).to have_content "Hello #{account.name}"
       end
