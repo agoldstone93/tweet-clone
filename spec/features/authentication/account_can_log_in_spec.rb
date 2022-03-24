ac3b87da9bbe2fe96c7c34e69314fa08400dc2d5
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-03-24 13:30:38 +0000

    add spec to validate visiting homepage

--- spec/features/authentication/account_can_log_in_spec.rb
+++ spec/features/authentication/account_can_log_in_spec.rb
@@ -12,6 +12,18 @@ RSpec.describe "Authentication", type: :feature do
           fill_in "Password", with: account.password
         end
         click_button "Log in"
+        expect(page).to have_current_path(root_path)
+      end
+    end
+    
+    context 'with valid log in details' do
+      it 'displays welcome message' do
+        visit new_account_session_path
+        within('#new_account') do
+          fill_in "Email", with: account.email
+          fill_in "Password", with: account.password
+        end
+        click_button "Log in"
         expect(page).to have_content "Hello #{account.name}"
       end
     end
