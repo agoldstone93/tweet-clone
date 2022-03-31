67746d133c14c353d1298bbc7d6098408f974068
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-03-31 14:49:26 +0100

    remove unused specs, add name and username to profile page, corresponding specs

--- spec/features/see_tweets_on_profile_page_spec.rb
+++ spec/features/see_tweets_on_profile_page_spec.rb
@@ -1,16 +1,23 @@
 require 'rails_helper'
 
-describe "Account is logged in" do
+describe "Account exists" do
   let(:account) { FactoryBot.create(:account) }
-  let!(:tweet1) { FactoryBot.create(:tweet, account: account, content: "this is a test") }
-  let!(:tweet2) { FactoryBot.create(:tweet, account: account, content: "this is another test tweet") }
+  let!(:tweet1) { FactoryBot.create(:tweet, account: account, content: "test first tweet") }
+  let!(:tweet2) { FactoryBot.create(:tweet, account: account, content: "test second tweet") }
 
-  context "Visit profile page" do
-    it "Can see most recent tweet" do
-      login_as(account, :scope => :account)
+  context "Navigate to profile page" do
+    it "Can see most recent tweets, reverse chronologically" do
+      login_as(account, scope: :account)
       visit profile_path
-      expect(page).to have_content ("this is a test")
-      expect(page).to have_content ("this is another test tweet")
+      expect(page).to have_selector("li:nth-child(2)", text: "test first tweet")
+      expect(page).to have_selector("li:nth-child(1)", text: "test second tweet")
+    end
+
+    it "Can see name and username" do
+      login_as(account, scope: :account)
+      visit profile_path
+      expect(page).to have_content("Name: #{account.name}")
+      expect(page).to have_content("Username: @#{account.username}")
     end
   end
 end
