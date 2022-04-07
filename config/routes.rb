c7791171735aacd0f83de807e25d09c326516d8c
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-04-07 17:32:57 +0100

    add create tweet page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get 'profile/index'
   get 'timeline/index'
-  root 'timeline#index'
   get 'timeline', to: "timeline#index"
   get 'profile', to: "profile#index"
+  root 'timeline#index'
+
+  resources :tweets
+  
   devise_for :accounts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
