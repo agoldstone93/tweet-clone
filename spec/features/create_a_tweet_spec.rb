f8510841c9be5807a7ed2520914f96d6af9e230a
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-04-14 14:02:45 +0100

    updating wording on spec

--- spec/features/create_a_tweet_spec.rb
+++ spec/features/create_a_tweet_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-describe "Account exists" do
+describe "Account is logged in" do
   let(:account) { FactoryBot.create(:account) }
   
-  context "Navigate to create tweet page" do
-    it "Can create a tweet and see it on timeline" do
+  context "Navigate to create tweet page and create tweet" do
+    it "I can see tweet on timeline" do
       login_as(account, scope: :account)
       visit new_tweet_path
       fill_in 'tweet_content', with: 'This is a test for writing a tweet'
@@ -12,6 +12,5 @@ describe "Account exists" do
       visit timeline_path
       expect(page).to have_content("This is a test for writing a tweet")
     end
-
   end
 end
