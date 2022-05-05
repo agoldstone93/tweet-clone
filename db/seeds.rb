c650824e6d5f424b54bc6c9b9a31f46d8e72d80e
Adam Goldstone <adam.goldstone@education.gov.uk>
2022-05-05 17:09:29 +0100

    create fake seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# delete previous data
+Account.destroy_all
+Tweet.destroy_all
+
+# generate accounts
+Account.create(name: 'User Person', username: 'user', email: 'user@example.com', password: 'user123')
+
+5.times do
+  name = Faker::Twitter.screen_name
+  Account.create(name: name, username: name, email: "#{name}@example.com", password: '123456')
+end
+
+# generate tweets
+30.times do
+  Tweet.create(content: Faker::Lorem.sentence(word_count: 3), account_id: rand(1..6))
+end
